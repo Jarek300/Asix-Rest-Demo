bdb18326bd608c26d662905f4f42bf3f95be9cd5
Jarosław Jasiński <jja@askom.com.pl>
2019-02-20 16:25:53 +0100

    Dodano stronę VariableDynamic - automatyczne odświeżanie wartości zmiennych

--- WebApplication/Pages/VariableDynamic/Demo1.cshtml.cs
+++ WebApplication/Pages/VariableDynamic/Demo1.cshtml.cs
@@ -1,25 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace WebApplication.Pages.VariableDynamic
 {
     public class VariableModel
     {
-        public string mValueFormatted, mReadError;
+        public VariableModel(string aName, string aDecription, string aUnit)
+        {
+            mName = aName;
+            mDecription = aDecription;
+            mUnit = aUnit;
+        }
+
+        public string mName, mDecription, mUnit;
+        public string mValueFormatted = "?", mReadError;
+        public DateTime mDateTime;
     }
 
 
     public class Demo1Model : PageModel
     {
-        public VariableModel mVariableModelA000;
+        public Dictionary<string, VariableModel> mVariableModelDict = new Dictionary<string, VariableModel>();
+        public VariableModel mVariableModelA000 = new VariableModel("A000", "Temperatura spalin przed odemglaczem", "°C");
+        public VariableModel mVariableModelA004 = new VariableModel("A004", "Temperatura kwasu siarkowego", "°C");
+        public VariableModel mVariableModelA008 = new VariableModel("A008", "Temperatura wody ciepłej", "°C");
+
+        public VariableModel mVariableModelA082 = new VariableModel("A082", "Przepływ kwasu siarkowego", "m3/h");
+        public VariableModel mVariableModelA084 = new VariableModel("A084", "Poziom w zb. cyrkulacyjnym kwasu", "%");
+        public VariableModel mVariableModelA086 = new VariableModel("A086", "Przepływ wody chłodzącej", "m3/h");
+
+        public Demo1Model()
+        {
+            mVariableModelDict.Add(mVariableModelA000.mName, mVariableModelA000);
+            mVariableModelDict.Add(mVariableModelA004.mName, mVariableModelA004);
+            mVariableModelDict.Add(mVariableModelA008.mName, mVariableModelA008);
+            mVariableModelDict.Add(mVariableModelA082.mName, mVariableModelA082);
+            mVariableModelDict.Add(mVariableModelA084.mName, mVariableModelA084);
+            mVariableModelDict.Add(mVariableModelA086.mName, mVariableModelA086);
+        }
 
         public void OnGet()
         {
-            mVariableModelA000 = ReadVariableValue();
+            ReadVariableValue(mVariableModelA000);
+            ReadVariableValue(mVariableModelA004);
+            ReadVariableValue(mVariableModelA008);
+
+            ReadVariableValue(mVariableModelA082);
+            ReadVariableValue(mVariableModelA084);
+            ReadVariableValue(mVariableModelA086);
         }
+
+
+
+        public PartialViewResult OnGetVar1(string name)
+        {
+            mVariableModelDict.TryGetValue(name, out VariableModel variableModel);
+            if (variableModel != null)
+            {
+                ReadVariableValue(variableModel);
+            }
+            else
+            {
+                variableModel = new VariableModel(name, "", "");
+                variableModel.mReadError = "Nieobsługiwana zmienna";
+            }
+
+            //return Partial("_Demo1Variable", mVariableModelA000);
+
+            return new PartialViewResult {
+                ViewName = "_Demo1Variable",
+                ViewData = new ViewDataDictionary<VariableModel>(ViewData, variableModel)
+            };
+        }
+
+
 
 
         class VariableState
@@ -34,21 +93,22 @@ namespace WebApplication.Pages.VariableDynamic
 
 
 
-        VariableModel ReadVariableValue()
+        void ReadVariableValue(VariableModel aVariableModel)
         {
-            VariableModel variableModel = new VariableModel();
-
             try
             {
                 HttpClient httpClient = new HttpClient();
                 httpClient.BaseAddress = new Uri("http://asport.askom.com.pl");
 
+                String uri = "/asix/v1/variable/value";
+                uri = QueryHelpers.AddQueryString(uri, "name", aVariableModel.mName);
 
-                HttpResponseMessage response = httpClient.GetAsync("/asix/v1/variable/value?name=A1101").Result;
+
+                HttpResponseMessage response = httpClient.GetAsync(uri).Result;
                 if (!response.IsSuccessStatusCode)
                 {
-                    variableModel.mReadError = "Błąd http: " + response.StatusCode.ToString();
-                    return variableModel;
+                    aVariableModel.mReadError = "Błąd http: " + response.StatusCode.ToString();
+                    return;
                 }             
 
 
@@ -58,44 +118,39 @@ namespace WebApplication.Pages.VariableDynamic
 
                 if (!variableState.readSucceeded)
                 {
-                    variableModel.mReadError = variableState.readStatusString;
-                    return variableModel;
+                    aVariableModel.mReadError = variableState.readStatusString;
+                    return;
                 }
 
+                aVariableModel.mDateTime = variableState.timeStamp;
 
                 switch (variableState.quality & 0xC0)
                 {
                     case 0xC0:
                     {
                         double value = (double)variableState.value;
-                        variableModel.mValueFormatted = value.ToString("F0");
+                        aVariableModel.mValueFormatted = value.ToString("F0");
                         break;
                     }
 
                     case 0x40:
                     {
                         double value = (double)variableState.value;
-                        variableModel.mValueFormatted = value.ToString("F0") + "?";
+                        aVariableModel.mValueFormatted = value.ToString("F0") + "?";
                         break;
                     }
 
                     default:
                     {
-                        variableModel.mValueFormatted = "?";
+                        aVariableModel.mValueFormatted = "?";
                         break;
                     }
                 }               
-
-                return variableModel;
             }
             catch (Exception e)
             {
-                variableModel.mReadError = e.Message;
-                return variableModel;
+                aVariableModel.mReadError = e.Message;
             }
-
         }
-
-
     }
 }
