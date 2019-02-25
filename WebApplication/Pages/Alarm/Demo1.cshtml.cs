1a63e875d582ee7d3c986166f3dccaf423808126
Jarosław Jasiński <jarek.jasinski@gmail.com>
2019-02-25 12:16:19 +0100

    WebApplication/Pages/Alarm/Demo1.cshtml.cs - Zaimplementowano wyświetlanie stanu kilku alarmów.

--- WebApplication/Pages/Alarm/Demo1.cshtml.cs
+++ WebApplication/Pages/Alarm/Demo1.cshtml.cs
@@ -1,17 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace WebApplication.Pages.Alarm
 {
+    public class AlarmModel
+    {
+        public AlarmModel(string aDomain, string aName)
+        {
+            mDomain = aDomain;
+            mName = aName;
+        }
+
+        public string mDomain, mName;
+        public AlarmState mAlarmState;
+    }
+
+
     public class Demo1Model : PageModel
     {
+        // 98, 108
+
+        public List<AlarmModel> mAlarmModelList = new List<AlarmModel>();
+
+
+        public Demo1Model()
+        {
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 011"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 083"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 098"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 109"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 155"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 202"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 227"));
+            mAlarmModelList.Add(new AlarmModel("Fabryka_EVO", "Alarm nr 'test'"));
+        }
+
         public void OnGet()
         {
+            ReadAlarmValue();
+        }
 
+
+        void ReadAlarmValue()
+        {
+            AsixRestClient asixRestClient = new AsixRestClient();
+
+            foreach (var alarmModel in mAlarmModelList)
+                alarmModel.mAlarmState = asixRestClient.ReadAlarmState(alarmModel.mDomain, alarmModel.mName);
         }
     }
+
 }
