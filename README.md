027f0123aafd482ec590648797096cf8607d2aaf
askomawi <Marcin.Witek@askom.pl>
2019-02-26 12:44:50 +0100

    Dodano przykład odświeżania wartości zmiennych przy użyciu ajax/json/jQuery.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Opis
+﻿# Opis
 [Asix.Evo](https://www.asix.com.pl) firmy [Askom](https://www.askom.pl) jest pakietem programowym klasy HMI/SCADA/MES umożliwiającym realizację komputerowych systemów wizualizacji, nadzoru i sterowania procesów przemysłowych.  Asix.Evo  ma możliwość udostępniania danych procesowych aplikacji za pośrednictwem serwera REST. Opis serwera REST jest dostepny [tutaj](https://www.askom.pl/WebHelp/Asix_Evo_9/AsixConnect_HTML5/index.htm#t=Serwer_REST_pakietu_Asix_Evo%2FHistoria_alarmow.htm).
 
 Projekt **Asix-AspNetMvcCore-Demo** zawiera przykłady pobierania danych procesowych z aplikacji Asix.Evo i wyświetlania ich na stronach Web. Aplikacja Web jest stworzona przy pomocy technologii Asp.Net MVC Core 2.2. Aby móc skompilować projekt należy zainstalować [Visual Studio 2017](https://visualstudio.microsoft.com/pl/downloads) oraz [.NET Core 2.2 SDK](https://dotnet.microsoft.com/download).
@@ -11,6 +11,7 @@ Zawartość projektu:
  * Variable/Demo2 - Odczyt wartości bieżącej wielu zmiennych
  * Variable/Demo3 - Odczyt wartości bieżącej wielu zmiennych, sygnalizacja przekroczeń limitów, sygnalizacja trendu zmian wartości
  * VariableDynamic/Demo1 - Odświeżanie wartości bieżącej wielu zmiennych bez przeładowywania całej strony
+ * VariableDynamic/Demo2 - Odświeżanie wartości bieżącej z użyciem ajax/json/jQuery
  * Archiwum/Demo1 - Odczyt wartości historycznych agregowanych dwu zmiennych i wyświetlenie w postaci tabeli
  * Archiwum/Demo2 - Odczyt wartości historycznych surowych jednej zmiennych i wyświetlenie w postaci wykresu
  * Alarm/Demo1 - Odczyt wartości bieżącej wielu alarmów
---
+++ WebApplication/Pages/VariableDynamic/Demo2.cshtml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace WebApplication.Pages.VariableDynamic
+{
+    public class VariableTemplate
+    {
+        public string mName;
+
+        public VariableTemplate(string aName)
+        {
+            mName = aName;
+        }
+    }
+
+
+    public class Demo2Model : PageModel
+    {
+        public Dictionary<string, VariableModel> mVariables = new Dictionary<string, VariableModel>();
+
+
+        public Demo2Model()
+        {
+            mVariables["A000"] = new VariableModel("A000", "Temperatura spalin przed odemglaczem", "°C");
+            mVariables["A004"] = new VariableModel("A004", "Temperatura kwasu siarkowego", "°C");
+            mVariables["A008"] = new VariableModel("A008", "Temperatura wody ciepłej", "°C");
+
+            mVariables["A082"] = new VariableModel("A082", "Przepływ kwasu siarkowego", "m³/h");
+            mVariables["A084"] = new VariableModel("A084", "Poziom w zb. cyrkulacyjnym kwasu", "%");
+            mVariables["A086"] = new VariableModel("A086", "Przepływ wody chłodzącej", "m³/h");
+
+            mVariables["Nieznana"] = new VariableModel("Nieznana", "Nieznana zmienna", "");
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+
+        public JsonResult OnGetVariables(string[] name)
+        {
+            if (name == null)
+                return null;
+
+            List<VariableModel> variables = new List<VariableModel>();
+            foreach (string variableName in name)
+            {
+                VariableModel variableModel = null;
+                if (!mVariables.ContainsKey(variableName))
+                    variableModel = new VariableModel(variableName, "", "");
+                else
+                    variableModel = mVariables[variableName];
+
+                ReadVariableValue(variableModel);
+                variables.Add(variableModel);
+            }
+
+            return new JsonResult(variables.ToArray());
+        }
+
+
+        void ReadVariableValue(VariableModel aVariableModel)
+        {
+            try
+            {
+                AsixRestClient asixRestClient = new AsixRestClient();
+                VariableState variableState = asixRestClient.ReadVariableState(aVariableModel.mName);
+
+                if (!variableState.readSucceeded)
+                {
+                    aVariableModel.mReadError = variableState.readStatusString;
+                    return;
+                }
+
+
+                aVariableModel.mDateTime = variableState.timeStamp;
+
+                switch (variableState.quality & 0xC0)
+                {
+                    case 0xC0:
+                        {
+                            double value = (double)variableState.value;
+                            aVariableModel.mValueFormatted = value.ToString("F0");
+                            break;
+                        }
+
+                    case 0x40:
+                        {
+                            double value = (double)variableState.value;
+                            aVariableModel.mValueFormatted = value.ToString("F0") + "?";
+                            break;
+                        }
+
+                    default:
+                        {
+                            aVariableModel.mValueFormatted = "?";
+                            break;
+                        }
+                }
+            }
+            catch (Exception e)
+            {
+                aVariableModel.mReadError = e.Message;
+            }
+        }
+
+    }
+}
