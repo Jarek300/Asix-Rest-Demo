7a7c280e464ed75b449c7fa7abe1da9208bf5bc4
Jarosław Jasiński <jarek.jasinski@gmail.com>
2019-02-26 16:09:52 +0100

    WebApplication/Pages/Variable/Demo2.cshtml - Dodano komentarz.

--- WebApplication/Pages/Variable/Demo2.cshtml.cs
+++ WebApplication/Pages/Variable/Demo2.cshtml.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication.Pages.Variable
 {
+    /// <summary>
+    /// Klasa przechowująca model jednej zmiennej
+    /// </summary>
     public class Demo2VariableModel
     {
         public Demo2VariableModel(string aName, string aDecription, string aUnit)
@@ -13,14 +16,34 @@ namespace WebApplication.Pages.Variable
             mUnit = aUnit;
         }
 
+        /// <summary>
+        ///  Atrybuty zmiennej
+        /// </summary>
         public string mName, mDecription, mUnit;
-        public string mValueFormatted = "?", mReadError;
+
+        /// <summary>
+        /// Sformatowaną wartość zmiennej
+        /// </summary>
+        public string mValueFormatted = "?";
+
+        /// <summary>
+        /// Ewentualny błąd odczytu wartości zmiennej
+        /// </summary>
+        public string mReadError;
     }
 
 
+
+    /// <summary>
+    /// Przykład odczytu wartości wielu zmiennych
+    /// </summary>
     public class Demo2Model : PageModel
     {
+        /// <summary>
+        /// Model zmiennej A000
+        /// </summary>
         public Demo2VariableModel mVariableModelA000 = new Demo2VariableModel("A000", "Temperatura spalin przed odemglaczem", "°C");
+
         public Demo2VariableModel mVariableModelA004 = new Demo2VariableModel("A004", "Temperatura kwasu siarkowego", "°C");
         public Demo2VariableModel mVariableModelA008 = new Demo2VariableModel("A008", "Temperatura wody ciepłej", "°C");
 
@@ -29,6 +52,9 @@ namespace WebApplication.Pages.Variable
         public Demo2VariableModel mVariableModelA086 = new Demo2VariableModel("A086", "Przepływ wody chłodzącej", "m³/h");
 
 
+        /// <summary>
+        /// Funkcja wywoływana przy generowaniu strony. Czyta wartosci kolejny zmiennych.
+        /// </summary>
         public void OnGet()
         {
             ReadVariableValue(mVariableModelA000);
@@ -42,14 +68,21 @@ namespace WebApplication.Pages.Variable
 
 
 
+        /// <summary>
+        /// Odczyt wartości jednej zmiennej
+        /// </summary>
+        /// <param name="aVariableModel">Model zmiennej. Jego pole mName zawiera nazwe zmiennej.</param>
         void ReadVariableValue(Demo2VariableModel aVariableModel)
         {
             try
             {
                 AsixRestClient asixRestClient = new AsixRestClient();
 
+                // Odczyt wartości zmiennej z serwera REST
                 VariableState variableState = asixRestClient.ReadVariableState(aVariableModel.mName);
 
+
+                // Obsług błędu odczytu
                 if (!variableState.readSucceeded)
                 {
                     aVariableModel.mReadError = variableState.readStatusString;
@@ -57,28 +90,24 @@ namespace WebApplication.Pages.Variable
                 }
 
 
-                switch (variableState.quality & 0xC0)
+                // Formatowanie wartości zmiennej
+                if (variableState.IsQualityGood())
                 {
-                    case 0xC0:
-                    {
-                        double value = (double)variableState.value;
-                        aVariableModel.mValueFormatted = value.ToString("F0");
-                        break;
-                    }
-
-                    case 0x40:
-                    {
-                        double value = (double)variableState.value;
-                        aVariableModel.mValueFormatted = value.ToString("F0") + "?";
-                        break;
-                    }
-
-                    default:
-                    {
-                        aVariableModel.mValueFormatted = "?";
-                        break;
-                    }
-                }               
+                    // Formatowanie wartości o jakości dobrej
+                    double value = (double)variableState.value;
+                    aVariableModel.mValueFormatted = value.ToString("F0");
+                }
+                else if (variableState.IsQualityUncertain())
+                {
+                    // Formatowanie wartości o jakości niepewnej
+                    double value = (double)variableState.value;
+                    aVariableModel.mValueFormatted = value.ToString("F0") + "?";
+                }
+                else
+                {
+                    // Dla wartości o jakości złej wyświetlimy pytajnik
+                    aVariableModel.mValueFormatted = "?";
+                }
             }
             catch (Exception e)
             {
