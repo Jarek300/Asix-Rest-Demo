9ffb1b05071cd224843061512f8f858558ec1808
JarJasAskom <jaroslaw.jasinski@askom.pl>
2023-03-23 11:38:50 +0100

    Refaktoryzacja. Variable/Demo1.cshtml.cs przechowuje teraz wartość zmiennej w polu klasy VariableModel co upraszcza uaktualnianie stanu po odczycie wartości zmiennej.

--- WebApplication/Pages/Variable/Demo1.cshtml.cs
+++ WebApplication/Pages/Variable/Demo1.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Asix;
-
+using WebApplication.Code;
 
 namespace WebApplication.Pages.Variable
 {
@@ -12,12 +12,7 @@ namespace WebApplication.Pages.Variable
         /// <summary>
         /// Przeczytana i sformatowana wartości zmiennej
         /// </summary>
-        public string mValueFormatted = "";
-
-        /// <summary>
-        /// Opis ewentualnego błędu odczytu
-        /// </summary>
-        public string mReadError = "";
+        public VariableModel VariableModel = new("A110", "Przepływ gazu do pieca", "m³/h");
 
 
         /// <summary>
@@ -27,49 +22,15 @@ namespace WebApplication.Pages.Variable
         {
             try
             {
-                await ReadVariableValue();
+                AsixRestClient asixRestClient = AsixRestClient.Create();
+
+                ICollection<VariableValue> variableValues = await asixRestClient.GetVariableValueAsync(new string[] { "A110" });
+                VariableValue variableState = variableValues.First();
+                VariableModel.SetVariableValue(variableState);
             }
             catch (Exception e)
             {
-                mReadError = e.Message;
-            }
-        }
-
-
-        async Task ReadVariableValue()
-        {
-            AsixRestClient asixRestClient = AsixRestClient.Create();
-
-            // Odczyt wartości zmiennej
-            ICollection<VariableValue> variableValues = await asixRestClient.GetVariableValueAsync(new string[] { "A110" });
-            VariableValue variableState = variableValues.First();
-
-
-            // Sprawdzenie czy nie wystąpił błąd odczytu
-            if (!variableState.ReadSucceeded)
-            {
-                mReadError = variableState.ReadStatusString;
-                return;
-            }
-
-
-            // Formatowanie wartości zmiennej
-            if (AsixRestClient.IsQualityGood(variableState.Quality))
-            {
-                // Formatowanie wartości o jakości dobrej
-                double value = (double)variableState.Value;
-                mValueFormatted = value.ToString("F0");
-            }
-            else if (AsixRestClient.IsQualityUncertain(variableState.Quality))
-            {
-                // Formatowanie wartości o jakości niepewnej
-                double value = (double)variableState.Value;
-                mValueFormatted = value.ToString("F0") + "?";
-            }
-            else
-            {
-                // Dla wartości o jakości złej wyświetlimy pytajnik
-                mValueFormatted = "?";
+                VariableModel.SetError(e.Message);
             }
         }
     }
