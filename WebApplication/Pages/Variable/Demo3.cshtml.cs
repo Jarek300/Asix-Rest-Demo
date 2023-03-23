128a7d4e366526c0f4f0c84bd7252cc5488a8f62
JarJasAskom <jaroslaw.jasinski@askom.pl>
2023-03-23 13:42:16 +0100

    Refaktoryzacja. Z Demo3.cshtml.cs klasę Demo3VariableModel przeniesiono do osobnego pliku i dodano w nim funkjcę SetValue. Dodano demo obsługi błędów przy odczycie nieznanej zmiennej.

--- WebApplication/Pages/Variable/Demo3.cshtml.cs
+++ WebApplication/Pages/Variable/Demo3.cshtml.cs
@@ -5,156 +5,6 @@ using Asix;
 namespace WebApplication.Pages.Variable
 {
     /// <summary>
-    /// Jakość zmiennej
-    /// </summary>
-    public enum ValueQuality { Good, Uncertain, Bad };
-
-    /// <summary>
-    /// Informacja o przekroczeniu limitów przez zmienną
-    /// </summary>
-    public enum ValueLimit { LoLo, Lo, Normal, Hi, HiHi };
-
-    /// <summary>
-    /// Trend zmian wartości zmiennej (wyliczany dla okresu ostatnich 15 minut)
-    /// </summary>
-    public enum ValueTrend { NotAvailable, Up, Down, NoChange };
-
-
-    /// <summary>
-    /// Klasa przechowująca model jednej zmiennej
-    /// </summary>
-    public class Demo3VariableModel
-    {
-        public Demo3VariableModel(string aName, string aDecription, string aUnit, string aDotNetFormat, double aLimitLoLo, double aLimitLo, double aLimitHi, double aLimitHiHi)
-        {
-            mName = aName;
-            mDecription = aDecription;
-            mUnit = aUnit;
-            mDotNetFormat = aDotNetFormat;
-
-            mLimitLoLo = aLimitLoLo;
-            mLimitLo = aLimitLo;
-            mLimitHi = aLimitHi;
-            mLimitHiHi = aLimitHiHi;
-        }
-
-
-        /// <summary>
-        ///  Atrybuty zmiennej
-        /// </summary>
-        public string mName, mDecription, mUnit;
-
-        /// <summary>
-        /// Format wartości zmiennej
-        /// </summary>
-        public string mDotNetFormat = "F0";
-
-        /// <summary>
-        /// Wartości limitów zmiennej
-        /// </summary>
-        public double mLimitLoLo, mLimitLo, mLimitHi, mLimitHiHi;
-
-        /// <summary>
-        /// Ewentualny błąd odczytu wartości zmiennej
-        /// </summary>
-        public string mReadError = "";
-
-        /// <summary>
-        /// Jakość zmiennej
-        /// </summary>
-        public ValueQuality mValueQuality = ValueQuality.Bad;
-
-        /// <summary>
-        /// Sformatowana wartość zmiennej
-        /// </summary>
-        public string mValueFormatted = "?";
-
-        /// <summary>
-        /// Sformatowana wartość średniej zmiennej za ostatnie 15 minut
-        /// </summary>
-        public string mAverageValueFormatted = "";
-
-        /// <summary>
-        /// Trend zmian wartości zmiennej
-        /// </summary>
-        public ValueTrend mValueTrend = ValueTrend.NotAvailable;
-
-        /// <summary>
-        /// Informacja o przekroczneiu limitów przez wartość zmiennej
-        /// </summary>
-        public ValueLimit mValueLimit = ValueLimit.Normal;
-
-
-        /// <summary>
-        /// Tworzenie modelu zmiennej - analiza jakośći wartość zmienej
-        /// </summary>
-        /// <param name="aOpcQuality">Jakość zmiennej</param>
-        public void SetValueQuality(int aOpcQuality)
-        {
-            switch (aOpcQuality & 0xC0)
-            {
-                case 0xC0:
-                    mValueQuality = ValueQuality.Good;
-                    break;
-
-                case 0x40:
-                    mValueQuality = ValueQuality.Uncertain;
-                    break;
-
-                default:
-                    mValueQuality = ValueQuality.Bad;
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Tworzenie modelu zmiennej - analiza wartości zmienej i wartości limitów
-        /// </summary>
-        /// <param name="aValue">Wartośc zmiennej</param>
-        public void SetFormatedValueAndValueLimit(object aValue)
-        {
-            double currentValue = Convert.ToDouble(aValue);
-            mValueFormatted = currentValue.ToString(mDotNetFormat);
-
-            if (currentValue <= mLimitLoLo)
-                mValueLimit = ValueLimit.LoLo;
-            else if (currentValue <= mLimitLo)
-                mValueLimit = ValueLimit.Lo;
-            else if (currentValue >= mLimitHiHi)
-                mValueLimit = ValueLimit.HiHi;
-            else if (currentValue >= mLimitHi)
-                mValueLimit = ValueLimit.Hi;
-            else
-                mValueLimit = ValueLimit.Normal;
-        }
-
-
-        /// <summary>
-        /// Tworzenie modelu zmiennej - analiza wartości wartość zmienej bieżącej i średniej - określenie trendu zmian wartości
-        /// </summary>
-        /// <param name="aValue">Wartość bieżąca zmiennej</param>
-        /// <param name="aAverageValue">Wartość średnia zmiennej</param>
-        public void SetValueTrend(object aValue, object aAverageValue)
-        {
-            double currentValue = Convert.ToDouble(aValue);
-            double averageValue = Convert.ToDouble(aAverageValue);
-
-            mAverageValueFormatted = averageValue.ToString(mDotNetFormat);
-
-            if (mValueFormatted == mAverageValueFormatted)
-                mValueTrend = ValueTrend.NoChange;
-            else
-            if (currentValue < averageValue)
-                mValueTrend = ValueTrend.Down;
-            else
-                mValueTrend = ValueTrend.Up;
-
-        }
-    }
-
-
-
-    /// <summary>
     /// Przykład odczytu wartości wielu zmiennych i wypracowania dodatkowych informacji o zmiennej - informacji o przekroczeniu limitów i trendu zmian wartości.
     /// Karta wartości zmiennej wyświetla informacje o przekroczeniu limitów w postaci koloru wartości, a informacje o trendzie zmian wartości w postaci ikony.
     /// </summary>
@@ -164,9 +14,11 @@ namespace WebApplication.Pages.Variable
         public Demo3VariableModel mVariableModelA004 = new Demo3VariableModel("A004", "Temperatura kwasu siarkowego", "°C", "F0", 2, 10, 190, 200);
         public Demo3VariableModel mVariableModelA008 = new Demo3VariableModel("A008", "Temperatura wody ciepłej", "°C", "F1", 1, 8, 90, 98);
 
-        public Demo3VariableModel mVariableModelA082 = new Demo3VariableModel("A082", "Przepływ kwasu siarkowego", "m³/h", "F0", 2, 10, 190, 200 );
+        public Demo3VariableModel mVariableModelA082 = new Demo3VariableModel("A082", "Przepływ kwasu siarkowego", "m³/h", "F0", 2, 10, 190, 200);
         public Demo3VariableModel mVariableModelA084 = new Demo3VariableModel("A084", "Poziom w zb. cyrkulacyjnym kwasu", "%", "F1", 2, 10, 190, 200);
         public Demo3VariableModel mVariableModelA086 = new Demo3VariableModel("A086", "Przepływ wody chłodzącej", "m³/h", "F0", 2, 10, 270, 290);
+
+        public Demo3VariableModel mVariableModelUnknown = new Demo3VariableModel("UnknownVariable", "UnknownVariable - desc", "m³/h", "F0", 2, 10, 270, 290);
 
 
         /// <summary>
@@ -181,42 +33,8 @@ namespace WebApplication.Pages.Variable
             await ReadVariableValue(mVariableModelA082);
             await ReadVariableValue(mVariableModelA084);
             await ReadVariableValue(mVariableModelA086);
-        }
 
-
-        /// <summary>
-        /// Funkcja używana do testowania poprawności generowania wizualizacji zmiennej w karcie
-        /// </summary>
-        public void testOnGet()
-        {
-            mVariableModelA000.mValueQuality = ValueQuality.Bad;
-
-            mVariableModelA004.mValueQuality = ValueQuality.Uncertain;
-            mVariableModelA004.mValueFormatted = "123,4";
-            mVariableModelA004.mValueLimit = ValueLimit.LoLo;
-            mVariableModelA004.mValueTrend = ValueTrend.NotAvailable;
-
-            mVariableModelA008.mValueQuality = ValueQuality.Good;
-            mVariableModelA008.mValueFormatted = "123";
-            mVariableModelA008.mValueLimit = ValueLimit.Lo;
-            mVariableModelA008.mValueTrend = ValueTrend.Down;
-
-            mVariableModelA082.mValueQuality = ValueQuality.Good;
-            mVariableModelA082.mValueFormatted = "11";
-            mVariableModelA082.mValueLimit = ValueLimit.Normal;
-            mVariableModelA082.mValueTrend = ValueTrend.Up;
-
-            mVariableModelA084.mValueQuality = ValueQuality.Good;
-            mVariableModelA084.mValueFormatted = "12";
-            mVariableModelA084.mValueLimit = ValueLimit.Normal;
-            mVariableModelA084.mValueTrend = ValueTrend.NoChange;
-
-
-            mVariableModelA086.mReadError = "Test błędu odczytu";
-            mVariableModelA086.mValueQuality = ValueQuality.Good;
-            mVariableModelA086.mValueFormatted = "-123";
-            mVariableModelA086.mValueLimit = ValueLimit.Normal;
-            mVariableModelA086.mValueTrend = ValueTrend.NoChange;
+            await ReadVariableValue(mVariableModelUnknown);
         }
 
 
@@ -231,39 +49,24 @@ namespace WebApplication.Pages.Variable
                 AsixRestClient asixRestClient = AsixRestClient.Create();
 
                 // Odczyt wartości zmiennej z serwera REST
-                ICollection<VariableValue> variableValues = await asixRestClient.GetVariableValueAsync(new string[] { aVariableModel.mName });
+                ICollection<VariableValue> variableValues = await asixRestClient.GetVariableValueAsync(new string[] { aVariableModel.Name });
                 VariableValue variableValue = variableValues.First();
-                if (!variableValue.ReadSucceeded)
-                {
-                    aVariableModel.mReadError = variableValue.ReadStatusString;
+
+                if (!aVariableModel.SetValue(variableValue))
                     return;
-                }
-
-
-                // wypracowanie przez klasę modelu informacji o jakości zmiennej
-                aVariableModel.SetValueQuality(variableValue.Quality);
-
-                if (aVariableModel.mValueQuality == ValueQuality.Bad)
-                    return;
-
-
-                // wypracowanie przez klasę modelu informacji o wartości zmiennej i limitach
-                aVariableModel.SetFormatedValueAndValueLimit(variableValue.Value);
 
 
                 // Odczyt średniej wartości zmiennej z serwera REST
-                ICollection<VariableValue> variableAverages = await asixRestClient.GetVariableAggregateAsync(aVariableModel.mName, "Average", "15m", null, null, "60S");
-                VariableValue variableAverage = variableAverages.First();
-                if (!variableAverage.ReadSucceeded || AsixRestClient.IsQualityBad(variableAverage.Quality))
-                    return;
+                ICollection<VariableValue> variableAverages = await asixRestClient.GetVariableAggregateAsync(aVariableModel.Name, "Average", "15m", null, null, "60S");
 
+                VariableValue variableAverage = variableAverages.First();
 
                 // wypracowanie przez klasę modelu informacji o trendzie zmian wartości zmiennej
-                aVariableModel.SetValueTrend(variableValue.Value, variableAverage.Value);
+                aVariableModel.SetValueTrend(variableAverage);
             }
             catch (Exception e)
             {
-                aVariableModel.mReadError = e.Message;
+                aVariableModel.ReadError = e.Message;
             }
         }
     }
---
+++ WebApplication/Pages/Variable/Demo3VariableModel.cs
@@ -0,0 +1,186 @@
+﻿using Asix;
+
+namespace WebApplication.Pages.Variable
+{
+    /// <summary>
+    /// Jakość zmiennej
+    /// </summary>
+    public enum ValueQuality { Good, Uncertain, Bad };
+
+    /// <summary>
+    /// Informacja o przekroczeniu limitów przez zmienną
+    /// </summary>
+    public enum ValueLimit { LoLo, Lo, Normal, Hi, HiHi };
+
+    /// <summary>
+    /// Trend zmian wartości zmiennej (wyliczany dla okresu ostatnich 15 minut)
+    /// </summary>
+    public enum ValueTrend { NotAvailable, Up, Down, NoChange };
+
+
+    /// <summary>
+    /// Klasa przechowująca model jednej zmiennej
+    /// </summary>
+    public class Demo3VariableModel
+    {
+        /// <summary>
+        ///  Atrybuty zmiennej
+        /// </summary>
+        public string Name, Decription, Unit;
+
+        /// <summary>
+        /// Format wartości zmiennej
+        /// </summary>
+        public string DotNetFormat = "F0";
+
+        /// <summary>
+        /// Wartości limitów zmiennej
+        /// </summary>
+        public double LimitLoLo, LimitLo, LimitHi, LimitHiHi;
+
+        /// <summary>
+        /// Ewentualny błąd odczytu wartości zmiennej
+        /// </summary>
+        public string ReadError = "";
+
+        /// <summary>
+        /// Jakość zmiennej
+        /// </summary>
+        public ValueQuality ValueQuality = ValueQuality.Bad;
+
+
+        /// <summary>
+        /// Wartość zmiennej
+        /// </summary>
+        public double Value = 0;
+
+        /// <summary>
+        /// Sformatowana wartość zmiennej
+        /// </summary>
+        public string ValueFormatted = "?";
+
+        /// <summary>
+        /// Sformatowana wartość średniej zmiennej za ostatnie 15 minut
+        /// </summary>
+        public string AverageValueFormatted = "";
+
+        /// <summary>
+        /// Trend zmian wartości zmiennej
+        /// </summary>
+        public ValueTrend ValueTrend = ValueTrend.NotAvailable;
+
+        /// <summary>
+        /// Informacja o przekroczneiu limitów przez wartość zmiennej
+        /// </summary>
+        public ValueLimit ValueLimit = ValueLimit.Normal;
+
+
+
+        public Demo3VariableModel(string aName, string aDecription, string aUnit, string aDotNetFormat, double aLimitLoLo, double aLimitLo, double aLimitHi, double aLimitHiHi)
+        {
+            Name = aName;
+            Decription = aDecription;
+            Unit = aUnit;
+            DotNetFormat = aDotNetFormat;
+
+            LimitLoLo = aLimitLoLo;
+            LimitLo = aLimitLo;
+            LimitHi = aLimitHi;
+            LimitHiHi = aLimitHiHi;
+        }
+
+
+        /// <summary>
+        /// Tworzenie modelu zmiennej
+        /// </summary>
+        public bool SetValue(VariableValue aVariableValue)
+        {
+            if (!aVariableValue.ReadSucceeded)
+            {
+                ReadError = aVariableValue.ReadStatusString;
+                return false;
+            }
+
+
+            SetValueQuality(aVariableValue.Quality);
+
+            if (ValueQuality == ValueQuality.Bad)
+                return false;
+
+            SetFormatedValueAndValueLimit(aVariableValue.Value);
+            return true;
+        }
+
+
+        /// <summary>
+        /// Tworzenie modelu zmiennej - analiza jakośći wartość zmienej
+        /// </summary>
+        /// <param name="aOpcQuality">Jakość zmiennej</param>
+        void SetValueQuality(int aOpcQuality)
+        {
+            switch (aOpcQuality & 0xC0)
+            {
+                case 0xC0:
+                    ValueQuality = ValueQuality.Good;
+                    break;
+
+                case 0x40:
+                    ValueQuality = ValueQuality.Uncertain;
+                    break;
+
+                default:
+                    ValueQuality = ValueQuality.Bad;
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Tworzenie modelu zmiennej - analiza wartości zmienej i wartości limitów
+        /// </summary>
+        /// <param name="aValue">Wartośc zmiennej</param>
+        void SetFormatedValueAndValueLimit(object aValue)
+        {
+            Value = Convert.ToDouble(aValue);
+            ValueFormatted = Value.ToString(DotNetFormat);
+
+            if (Value <= LimitLoLo)
+                ValueLimit = ValueLimit.LoLo;
+            else if (Value <= LimitLo)
+                ValueLimit = ValueLimit.Lo;
+            else if (Value >= LimitHiHi)
+                ValueLimit = ValueLimit.HiHi;
+            else if (Value >= LimitHi)
+                ValueLimit = ValueLimit.Hi;
+            else
+                ValueLimit = ValueLimit.Normal;
+        }
+
+
+        /// <summary>
+        /// Tworzenie modelu zmiennej - analiza wartości wartość zmienej bieżącej i średniej - określenie trendu zmian wartości
+        /// </summary>
+        /// <param name="aValue">Wartość bieżąca zmiennej</param>
+        /// <param name="aAverageValue">Wartość średnia zmiennej</param>
+        public void SetValueTrend(VariableValue aVariableAverage)
+        {
+            if (!aVariableAverage.ReadSucceeded || AsixRestClient.IsQualityBad(aVariableAverage.Quality))
+            {
+                ValueTrend = ValueTrend.NotAvailable;
+                return;
+            }
+
+            double averageValue = Convert.ToDouble(aVariableAverage.Value);
+
+            AverageValueFormatted = averageValue.ToString(DotNetFormat);
+
+            if (ValueFormatted == AverageValueFormatted)
+                ValueTrend = ValueTrend.NoChange;
+            else
+            if (Value < averageValue)
+                ValueTrend = ValueTrend.Down;
+            else
+                ValueTrend = ValueTrend.Up;
+        }
+    }
+}
