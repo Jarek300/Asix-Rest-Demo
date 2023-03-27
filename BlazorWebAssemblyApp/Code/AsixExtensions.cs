782ef359a09af88b63e68a8f7a8d5810b5ce8934
JarJasAskom <jaroslaw.jasinski@askom.pl>
2023-03-27 15:10:51 +0200

    Dodano nowy projekt demo typu Blazor WebAssembly. Dodano stroną wyświetlającą wartości bieżące zmiennych.

---
+++ BlazorWebAssemblyApp/Code/AsixExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace Asix;
+
+public partial class AsixRestClient
+{
+    public readonly static string AlarmDomainName = "Fabryka_EVO";
+    public readonly static string RestServerUrl = "http://asport.askom.pl/asix";
+    public readonly static string RestServerUrlHttps = "https://asport.askom.pl/asix";
+
+    public static AsixRestClient Create()
+    {
+        HttpClient httpClient = new HttpClient();
+
+        AsixRestClient asixRestClient = new AsixRestClient(httpClient);
+        asixRestClient.BaseUrl = RestServerUrl;
+        return asixRestClient;
+    }
+
+
+    public static bool IsQualityGood(int aQuality)
+    {
+        return (aQuality & 0xC0) == 0xC0;
+    }
+
+
+    /// <summary>
+    /// Funkcja pozwala sprawdzić czy jakość zmiennej jest niepewna
+    /// </summary>
+    public static bool IsQualityUncertain(int aQuality)
+    {
+        return (aQuality & 0xC0) == 0x40;
+    }
+
+
+    /// <summary>
+    /// Funkcja pozwala sprawdzić czy jakość zmiennej jest zła
+    /// </summary>
+    public static bool IsQualityBad(int aQuality)
+    {
+        return (aQuality & 0xC0) == 0;
+    }
+}
+
+
+public partial class AggregateRange
+{
+    public AggregateRange(string aName, string aAggregate, string aPeriodLength, string aRefreshInterval)
+    {
+        Name = aName;
+        Aggregate = aAggregate;
+        PeriodLength = aPeriodLength;
+        RefreshInterval = aRefreshInterval;
+    }
+}
---
+++ BlazorWebAssemblyApp/Code/VariableModel.cs
@@ -0,0 +1,91 @@
+﻿using Asix;
+
+
+namespace WebApplication.Code
+{
+    /// <summary>
+    /// Klasa przechowująca model jednej zmiennej
+    /// </summary>
+
+    public class VariableModel
+    {
+        /// <summary>
+        ///  Atrybuty zmiennej
+        /// </summary>
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// Ewentualny błąd odczytu wartości zmiennej
+        /// </summary>
+        public string ReadError { get; set; } = "";
+
+
+        /// <summary>
+        /// Sformatowaną wartość zmiennej
+        /// </summary>
+        public string ValueFormatted { get; set; } = "?";
+
+        /// <summary>
+        /// Stemple czasu wartości zmiennej
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+
+        public VariableModel(string aName, string aDecription, string aUnit)
+        {
+            Name = aName;
+            Decription = aDecription;
+            Unit = aUnit;
+        }
+
+
+        public void SetVariableValue(VariableValue aVariableValue)
+        {
+            try
+            {
+                if (!aVariableValue.ReadSucceeded)
+                {
+                    ReadError = aVariableValue.ReadStatusString;
+                    return;
+                }
+
+                DateTime = aVariableValue.TimeStamp;
+
+                switch (aVariableValue.Quality & 0xC0)
+                {
+                    case 0xC0:
+                        {
+                            double value = (double)aVariableValue.Value;
+                            ValueFormatted = value.ToString("F0");
+                            break;
+                        }
+
+                    case 0x40:
+                        {
+                            double value = (double)aVariableValue.Value;
+                            ValueFormatted = value.ToString("F0") + "?";
+                            break;
+                        }
+
+                    default:
+                        {
+                            ValueFormatted = "?";
+                            break;
+                        }
+                }
+            }
+            catch (Exception e)
+            {
+                ReadError = e.Message;
+            }
+        }
+
+
+        internal void SetError(string aReadError)
+        {
+            ReadError = aReadError;
+        }
+    }
+}
